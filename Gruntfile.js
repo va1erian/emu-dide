77351b5a156a9a5b4a45e647081731faafa55a15
Hadrien Bellahcene <hello@graphes.at>
2014-02-26 00:10:24 +0100

    Setting up build system

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower: {
+      dev : {
+        dest: 'build/',
+        js_dest: 'build/js',
+        css_dest: 'build/css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/js/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bower');
+
+  grunt.registerTask('default', ['bower']);
+
+};
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 emu-dide
 ========
 
-This is the repository for our Web Dev finals project. Our goal is to provides an online assembler and 
-emulator for the DIDE architecture as seen in our Architacture classes at Polytech Paris Sud. 
+This is the repository for our Web Dev finals project. Our goal is to provide an online assembler and 
+emulator for the DIDE architecture as seen in our Architecture classes at Polytech Paris Sud. 
 
 More details will be available later.
