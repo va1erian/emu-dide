6361b5dae62a8bc971e3bea8fb55f4bf427fe54d
cedossan <cedric.dossantos.91@gmail.com>
2014-05-28 18:17:03 +0200

    Debugger memory editor fix. Some work on web storage.

---
+++ scripts/db_emudide.sql
@@ -0,0 +1,22 @@
+
+-- Table: app_user
+CREATE TABLE app_user ( 
+    id       INTEGER        PRIMARY KEY AUTOINCREMENT
+                            NOT NULL,
+    nickname VARCHAR( 30 )  NOT NULL,
+    passhash VARCHAR        NOT NULL 
+);
+
+
+-- Table: app_sourcecode
+CREATE TABLE app_sourcecode ( 
+    id          INTEGER  PRIMARY KEY AUTOINCREMENT
+                         NOT NULL,
+    app_user_id INTEGER  NOT NULL
+                         REFERENCES app_user ( id ),
+    name        VARCHAR  NOT NULL
+                         UNIQUE,
+    last_update DATETIME NOT NULL,
+    content     TEXT     NOT NULL 
+);
+
