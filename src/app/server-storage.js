5551d56f5630e548c43f022ec158cf5f264792b3
Hadrien Bellahcene <smiloo@gmail.com>
2014-05-29 00:05:45 +0200

    Client side web storage management - log in, etc.

--- src/app/server-storage.js
+++ src/app/server-storage.js
@@ -12,7 +12,7 @@ ServerStorage = (function(){
     
     var identity; //undefined if not logged in
     
-    function renderProgramList() {
+    function fetchPrograms() {
         
         $.getJSON('/programs', function() {
             console.log('fetched program list');
@@ -20,9 +20,9 @@ ServerStorage = (function(){
             console.log(data);
             $('#program-list').empty();
             
-            _(data).each(function(i) {
+            _(data.sources).each(function(i) {
                 var el = $(programItemTemplate({program : i }));
-                el.click(function() { alert(i.name);});
+                el.click(function() { fetchProgram(i.id); } );
                 $('#program-list').append(el);
             });
             
@@ -33,10 +33,54 @@ ServerStorage = (function(){
         
     }
     
-    pub.renderProgramList = renderProgramList;
+    function fetchProgram(id) {
+        var req = $.getJSON('/programs/' + id);
+        
+        req.done(function(data) {
+            console.log(data);
+        });
+        
+        req.fail(function() {
+           alert('Sorry, an error when attempting to fetch the program.'); 
+        });
+    }
+    
+    function processLogInForm(e)  {
+        e.preventDefault();
+        
+        var nick = $('#login-username-field').val();
+        var pwd  = $('#login-password-field').val();
+        var url  = $('#login-form').attr('action');
+        
+        var query = $.post(url, {'nickname':  nick, 'password': pwd});
+        
+        query.done(function(data) {
+           if(data.status === 'ok') {
+                identity = data.user;
+                fetchPrograms();
+                UI.switchSidePane(UI.SIDE_PANE.PROGRAMS);
+                $(pub).trigger('loggedIn');
+            }
+        });
+        
+        query.fail(function() {
+            alert('Sorry, an error occured during the log-in attempt');
+        });
+    }
+    
+    pub.renderProgramList = fetchPrograms;
     
     pub.init = function () {
-        
+        $('#login-form').submit(processLogInForm);
+    };
+    
+    pub.isLoggedIn = function () {
+        //return identity !== undefined;
+        return true;
+    };
+    
+    pub.userIdentity = function() {
+        return identity;
     };
     
     
